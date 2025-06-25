a6e85358f3ef6b32189ccfe79cbd5b4a78354bf3
ShahaliCodes <shahali44639@gmail.com>
2025-06-25 16:41:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Results from './pages/Results';
+import ErrorFallback from './components/ErrorFallback'; // Create this component
+import { ErrorBoundary } from 'react-error-boundary';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </ErrorBoundary>
     </div>
   );
 }
-
-export default App;
